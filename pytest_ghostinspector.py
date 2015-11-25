9bd58b7bb253d627fb6f15ecf53375ee0dd79d26
Jay Luker <lbjay@reallywow.com>
2015-11-25 11:51:12 -0500

    allow test input params on command line

--- pytest_ghostinspector.py
+++ pytest_ghostinspector.py
@@ -5,9 +5,13 @@ import pytest
 import shutil
 import requests
 import tempfile
+from os import getenv as env
 from contextlib import contextmanager
 
-GI_API_URL = 'https://api.ghostinspector.com/v1/'
+API_URL = env('GI_API_URL', 'https://api.ghostinspector.com/v1/')
+API_KEY = env('GI_API_KEY')
+START_URL = env('GI_START_URL')
+
 
 # Command-line Options
 
@@ -17,12 +21,14 @@ def pytest_addoption(parser):
         '--gi_key',
         action='store',
         dest='gi_key',
+        default=API_KEY,
         help='Set the value for the Ghost Inspector API key'
     )
     group.addoption(
         '--gi_start_url',
         action='store',
         dest='gi_start_url',
+        default=START_URL,
         help='Override the starting url value for the Ghost Inspector tests'
     )
     group.addoption(
@@ -44,10 +50,11 @@ def pytest_addoption(parser):
         '--gi_param',
         action='append',
         dest='gi_param',
-        help='Querystring param (repeatable) to include in the API execute request'
+        help=(
+            'Querystring param (repeatable) to include '
+            'in the API execute request. Example: "--gi_param foo=bar"'
+        )
     )
-
-    parser.addini('gi_key', 'Dummy pytest.ini setting')
 
 # Plugin hook impls
 
@@ -79,7 +86,8 @@ def pytest_collection(session):
         tf.close()
         return tf.name
 
-    if not session.config.option.gi_suite and not session.config.option.gi_test:
+    if not session.config.option.gi_suite \
+            and not session.config.option.gi_test:
         yield
     else:
         with _make_tmp_dir() as tmpdir:
@@ -125,14 +133,9 @@ class GIYamlCollector(pytest.File, GIAPIMixin):
     def __init__(self, *args, **kwargs):
         super(GIYamlCollector, self).__init__(*args, **kwargs)
         self.gi_key = self.config.option.gi_key
-        self.gi_param_options = []
 
     def collect(self):
         raw = yaml.safe_load(self.fspath.open())
-
-        # globally set test execution params
-        if 'param_options' in raw:
-            self.gi_param_options = raw['param_options']
 
         for suite in raw.get('suites', []):
             for test_item in self._collect_suite(suite):
@@ -142,40 +145,28 @@ class GIYamlCollector(pytest.File, GIAPIMixin):
             yield self._collect_test(test)
 
     def _collect_suite(self, suite):
-        url = GI_API_URL + 'suites/{}/tests/'.format(suite['id'])
+        url = API_URL + 'suites/{}/tests/'.format(suite['id'])
         test_list = self._api_request(url)
-        for test in test_list:
-            spec = {
-                'id': test['_id'],
-                'suite': test['suite']['name'],
-                'params': self._spec_param_opts(suite)
-            }
-            yield GITestItem(test['name'], self, spec)
+        for test_config in test_list:
+            yield self._create_test_item(test_config)
 
     def _collect_test(self, test):
-        url = GI_API_URL + 'tests/{}/'.format(test['id'])
-        test_data = self._api_request(url)
-        spec = {
-            'id': test_data['_id'],
-            'suite': test_data['suite']['name'],
-            'params': self._spec_param_opts(test)
-        }
-        return GITestItem(test_data['name'], self, spec)
+        url = API_URL + 'tests/{}/'.format(test['id'])
+        test_config = self._api_request(url)
+        return self._create_test_item(test_config)
 
-    def _spec_param_opts(self, node):
-        spec_params = {}
-        param_opts = self.gi_param_options
-        if 'param_options' in node:
-            param_opts.extend(node['param_options'])
-        for param_opt in param_opts:
-            try:
-                spec_params[param_opt] = getattr(self.config.option, 'gi_' + param_opt)
-            except AttributeError:
-                raise pytest.UsageError(
-                    "Unknown command-line option specified in param_options: gi_%s" % param_opt)
-        return spec_params
+    def _create_test_item(self, test_config):
+
+        params = dict(x.split('=') for x in self.config.option.gi_param)
+        spec = {
+            'id': test_config['_id'],
+            'suite': test_config['suite']['name'],
+            'params': params
+        }
+        return GITestItem(test_config['name'], self, spec)
 
 class GITestItem(pytest.Item, GIAPIMixin):
+
 
     def __init__(self, name, parent, spec):
         super(GITestItem, self).__init__(name, parent)
@@ -183,7 +174,7 @@ class GITestItem(pytest.Item, GIAPIMixin):
         self.spec = spec
 
     def runtest(self):
-        url = GI_API_URL + 'tests/{}/execute/'.format(self.spec['id'])
+        url = API_URL + 'tests/{}/execute/'.format(self.spec['id'])
         result = self._api_request(url, self.spec['params'])
         if not result['passing']:
             raise GIException(self, result)
@@ -192,11 +183,14 @@ class GITestItem(pytest.Item, GIAPIMixin):
         """ format failure info from GI API response """
         if isinstance(excinfo.value, GIException):
             resp_data = excinfo.value.args[1]
-            failing_step = next(step for step in resp_data['steps'] if not step['passing'])
+            failing_step = next(
+                step for step in resp_data['steps'] if not step['passing']
+            )
+            result_url_base = 'https://app.ghostinspector.com/results/'
             return "\n".join([
                 "Ghost Inspector test failed",
                 "   name: %s" % resp_data['test']['name'],
-                "   result url: https://app.ghostinspector.com/results/%s" % resp_data['_id'],
+                "   result url: %s/%s" % (result_url_base, resp_data['_id']),
                 "   sequence: %d" % failing_step['sequence'],
                 "   target: %s" % failing_step['target'],
                 "   command: %s" % failing_step['command'],
